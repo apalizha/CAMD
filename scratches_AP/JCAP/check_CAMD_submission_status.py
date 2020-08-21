e053ec3334ccaa41c779fc089d428f7f6c781ccc
Ubuntu <ubuntu@ip-172-31-53-189.ec2.internal>
2020-08-21 14:09:11 +0000

    deleted the old scratch files

--- scratches_AP/JCAP/check_CAMD_submission_status.py
+++
@@ -1,8 +0,0 @@
-from camd_apigw import s3_utils
-from tqdm import tqdm
-with open('submitted_combination.log') as f:
-    submissions  = f.readlines()
-submitted_candidates = [data.split(' : ')[1].strip('\n') for data in submissions]
-print('Total of {} systems submitted, status are below ....'.format(len(submitted_candidates)))
-for chemsys in submitted_candidates:
-    print(chemsys, ":",  s3_utils.get_run_status(chemsys))
